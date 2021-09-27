220838d316bbab9b8ccfb810f899db8b8ec6e28b
Bastien BORDEREAU <bastien.bordereau@gmail.com>
2021-09-27 23:03:52 +0200

    modif norme

--- Probleme 2/main.py
+++ Probleme 2/main.py
@@ -1,7 +1,7 @@
 from time import sleep
 import sys
-from termcolor import colored, cprint
-from random import randint
+from termcolor import colored
+
 
 def stoa(name):
     with open(name, 'r') as ppm:
@@ -9,18 +9,14 @@ def stoa(name):
     data = data.split("\n")
     return (data)
 
+
 string = [stoa("p/s.txt"), stoa("p/a.txt"), stoa("p/l.txt")]
 c = -1
 for i in range(len(string[0])-1):
-    c +=1
+    c += 1
     for j in range(len(string)):
         colors = ["red", "yellow", "green", "cyan", "blue", "magenta", "white"]
-        print(colored(string[j][i], colors[c]), end = "")
-        
+        print(colored(string[j][i], colors[c]), end="")
         sys.stdout.flush()
     sleep(0.05)
     print('\r')
-
-
-
-
